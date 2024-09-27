9c93b1c45c61568b8c605d5054cfba8cb2b3fe82
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-09-27 17:18:49 +0530

    Seventy Three point 3

--- src/App.jsx
+++ src/App.jsx
@@ -33,199 +33,199 @@ import AddEmployees from "./pages/AddEmployees";
 import AllEmployees from "./pages/AllEmployees";
 
 function App() {
-  const { loading } = useSelector((state) => state.alerts);
+  // const { loading } = useSelector((state) => state.alerts);
 
   return (
     <BrowserRouter>
-      {loading ? (
+      {/* {loading ? (
         <Spinner />
-      ) : (
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route
-            path="/about"
-            element={
-              <PublicRoute>
-                <About />
-              </PublicRoute>
-            }
-          ></Route>
-          <Route path="/login" element={<Login />}></Route>
+      ) : ( */}
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route
+          path="/about"
+          element={
+            <PublicRoute>
+              <About />
+            </PublicRoute>
+          }
+        ></Route>
+        <Route path="/login" element={<Login />}></Route>
 
-          <Route path="/privacy" element={<Privacy />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-          <Route path="/forgotPassword" element={<ForgotPassword />}></Route>
-          <Route
-            path="/resetPassword/:id/:token"
-            element={<ResetPassword />}
-          ></Route>
+        <Route path="/privacy" element={<Privacy />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/forgotPassword" element={<ForgotPassword />}></Route>
+        <Route
+          path="/resetPassword/:id/:token"
+          element={<ResetPassword />}
+        ></Route>
 
-          <Route path="/pricing" element={<Pricing />}></Route>
+        <Route path="/pricing" element={<Pricing />}></Route>
 
-          <Route path="/contact" element={<Contact />}></Route>
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          ></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        ></Route>
 
-          <Route
-            path="/admin/users"
-            element={
-              <ProtectedRoute>
-                <Users />
-              </ProtectedRoute>
-            }
-          ></Route>
+        <Route
+          path="/admin/users"
+          element={
+            <ProtectedRoute>
+              <Users />
+            </ProtectedRoute>
+          }
+        ></Route>
 
-          <Route
-            path="/admin/facilitators"
-            element={
-              <ProtectedRoute>
-                <Facilitators />
-              </ProtectedRoute>
-            }
-          ></Route>
+        <Route
+          path="/admin/facilitators"
+          element={
+            <ProtectedRoute>
+              <Facilitators />
+            </ProtectedRoute>
+          }
+        ></Route>
 
-          <Route
-            path="/admin/organizers"
-            element={
-              <ProtectedRoute>
-                <Organizers />
-              </ProtectedRoute>
-            }
-          ></Route>
+        <Route
+          path="/admin/organizers"
+          element={
+            <ProtectedRoute>
+              <Organizers />
+            </ProtectedRoute>
+          }
+        ></Route>
 
-          <Route
-            path="/facilitator/organizers"
-            element={
-              <ProtectedRoute>
-                <FaOrganizers />
-              </ProtectedRoute>
-            }
-          ></Route>
+        <Route
+          path="/facilitator/organizers"
+          element={
+            <ProtectedRoute>
+              <FaOrganizers />
+            </ProtectedRoute>
+          }
+        ></Route>
 
-          <Route
-            path="/participants"
-            element={
-              <ProtectedRoute>
-                <Participants />
-              </ProtectedRoute>
-            }
-          ></Route>
+        <Route
+          path="/participants"
+          element={
+            <ProtectedRoute>
+              <Participants />
+            </ProtectedRoute>
+          }
+        ></Route>
 
-          <Route
-            path="/profile/:id"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          ></Route>
+        <Route
+          path="/profile/:id"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        ></Route>
 
-          <Route
-            path="/facilitators/createSchedule"
-            element={
-              <ProtectedRoute>
-                <ScheduleCreate />
-              </ProtectedRoute>
-            }
-          ></Route>
+        <Route
+          path="/facilitators/createSchedule"
+          element={
+            <ProtectedRoute>
+              <ScheduleCreate />
+            </ProtectedRoute>
+          }
+        ></Route>
 
-          <Route
-            path="/existingSchedules/:id"
-            element={
-              <ProtectedRoute>
-                <ExistingSchedules />
-              </ProtectedRoute>
-            }
-          ></Route>
+        <Route
+          path="/existingSchedules/:id"
+          element={
+            <ProtectedRoute>
+              <ExistingSchedules />
+            </ProtectedRoute>
+          }
+        ></Route>
 
-          <Route
-            path="/addEmployees/:id"
-            element={
-              <ProtectedRoute>
-                <AddEmployees />
-              </ProtectedRoute>
-            }
-          ></Route>
+        <Route
+          path="/addEmployees/:id"
+          element={
+            <ProtectedRoute>
+              <AddEmployees />
+            </ProtectedRoute>
+          }
+        ></Route>
 
-          <Route
-            path="/allEmployees/:id"
-            element={
-              <ProtectedRoute>
-                <AllEmployees />
-              </ProtectedRoute>
-            }
-          ></Route>
+        <Route
+          path="/allEmployees/:id"
+          element={
+            <ProtectedRoute>
+              <AllEmployees />
+            </ProtectedRoute>
+          }
+        ></Route>
 
-          <Route
-            path="/userSchedules/:id"
-            element={
-              <ProtectedRoute>
-                <UserSchedules />
-              </ProtectedRoute>
-            }
-          ></Route>
+        <Route
+          path="/userSchedules/:id"
+          element={
+            <ProtectedRoute>
+              <UserSchedules />
+            </ProtectedRoute>
+          }
+        ></Route>
 
-          <Route
-            path="/book-appointment/:id"
-            element={
-              <ProtectedRoute>
-                <BookingPage />
-              </ProtectedRoute>
-            }
-          ></Route>
+        <Route
+          path="/book-appointment/:id"
+          element={
+            <ProtectedRoute>
+              <BookingPage />
+            </ProtectedRoute>
+          }
+        ></Route>
 
-          <Route
-            path="/myAvailability/:id"
-            element={
-              <ProtectedRoute>
-                <MyAvailability />
-              </ProtectedRoute>
-            }
-          ></Route>
+        <Route
+          path="/myAvailability/:id"
+          element={
+            <ProtectedRoute>
+              <MyAvailability />
+            </ProtectedRoute>
+          }
+        ></Route>
 
-          <Route
-            path="/book/:id"
-            element={
-              <ProtectedRoute>
-                <Book />
-              </ProtectedRoute>
-            }
-          ></Route>
+        <Route
+          path="/book/:id"
+          element={
+            <ProtectedRoute>
+              <Book />
+            </ProtectedRoute>
+          }
+        ></Route>
 
-          <Route
-            path="/share/:id"
-            element={
-              <PublicRoute>
-                <ShareSchedule />
-              </PublicRoute>
-            }
-          ></Route>
+        <Route
+          path="/share/:id"
+          element={
+            <PublicRoute>
+              <ShareSchedule />
+            </PublicRoute>
+          }
+        ></Route>
 
-          <Route
-            path="/userBook/:id/:timeSlot/:slotId"
-            element={
-              <PublicRoute>
-                <UserBooking />
-              </PublicRoute>
-            }
-          ></Route>
+        <Route
+          path="/userBook/:id/:timeSlot/:slotId"
+          element={
+            <PublicRoute>
+              <UserBooking />
+            </PublicRoute>
+          }
+        ></Route>
 
-          <Route
-            path="/notifications"
-            element={
-              <ProtectedRoute>
-                <Notifications />
-              </ProtectedRoute>
-            }
-          ></Route>
-        </Routes>
-      )}
+        <Route
+          path="/notifications"
+          element={
+            <ProtectedRoute>
+              <Notifications />
+            </ProtectedRoute>
+          }
+        ></Route>
+      </Routes>
+      {/* )} */}
     </BrowserRouter>
   );
 }
